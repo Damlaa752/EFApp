3084ff9120f01002d27bbf3748244df84322f1fa
Damlaa752 <gultekindamla7@gmail.com>
2023-04-04 14:14:56 +0300

    Add project files.

---
+++ 1_DatabaseFirstApp/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace _1_DatabaseFirstApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ekleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.değiştirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtCountry);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtCity);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtSurname);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Location = new System.Drawing.Point(363, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 227);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(61, 182);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Kaydet";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(20, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 15);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Ülke :";
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(82, 148);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(100, 20);
+            this.txtCountry.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(20, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Şehir :";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(82, 104);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(20, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Soyad :";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(82, 64);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(100, 20);
+            this.txtSurname.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(20, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ad :";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(82, 25);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // dgv
+            // 
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv.BackgroundColor = System.Drawing.Color.Firebrick;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.ContextMenuStrip = this.contextMenuStrip1;
+            this.dgv.Location = new System.Drawing.Point(12, 69);
+            this.dgv.MultiSelect = false;
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv.Size = new System.Drawing.Size(326, 275);
+            this.dgv.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ekleToolStripMenuItem,
+            this.değiştirToolStripMenuItem,
+            this.çıkarToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(115, 70);
+            // 
+            // ekleToolStripMenuItem
+            // 
+            this.ekleToolStripMenuItem.Name = "ekleToolStripMenuItem";
+            this.ekleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ekleToolStripMenuItem.Text = "Ekle";
+            this.ekleToolStripMenuItem.Click += new System.EventHandler(this.ekleToolStripMenuItem_Click);
+            // 
+            // değiştirToolStripMenuItem
+            // 
+            this.değiştirToolStripMenuItem.Name = "değiştirToolStripMenuItem";
+            this.değiştirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.değiştirToolStripMenuItem.Text = "Değiştir";
+            this.değiştirToolStripMenuItem.Click += new System.EventHandler(this.değiştirToolStripMenuItem_Click);
+            // 
+            // çıkarToolStripMenuItem
+            // 
+            this.çıkarToolStripMenuItem.Name = "çıkarToolStripMenuItem";
+            this.çıkarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.çıkarToolStripMenuItem.Text = "Çıkar";
+            this.çıkarToolStripMenuItem.Click += new System.EventHandler(this.çıkarToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ara";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(99, 32);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 20);
+            this.txtSearch.TabIndex = 0;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(12, 352);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(75, 23);
+            this.btnListele.TabIndex = 1;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 387);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        public System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.ToolStripMenuItem ekleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem değiştirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkarToolStripMenuItem;
+    }
+}
+
---
+++ 1_DatabaseFirstApp/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_DatabaseFirstApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            groupBox1.Visible = false;
+        }
+        NorthwindEntities1 db = new NorthwindEntities1();
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            //dgv.DataSource = db.Employees.ToList();
+            GridFill();
+        }
+        bool isAdd;
+        private void ekleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = true ; 
+            isAdd = true ;
+            groupBox1.Text = "Ekle" ;
+        }
+
+        private void değiştirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            id = (int)dgv.CurrentRow.Cells[0].Value;
+            groupBox1.Visible = true;
+            isAdd = false;
+            groupBox1.Text = "Güncelleştir";
+            id = (int)dgv.CurrentRow.Cells[0].Value;
+            txtName.Text = dgv.CurrentRow.Cells[1].Value.ToString();
+            txtSurname.Text = dgv.CurrentRow.Cells[2].Value.ToString();
+            txtCity.Text = dgv.CurrentRow.Cells[3].Value.ToString();
+            txtCountry.Text = dgv.CurrentRow.Cells[4].Value.ToString();
+        }
+        int id;
+        private void çıkarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            id= (int)dgv.CurrentRow.Cells[0].Value;
+            Employees employee = db.Employees.Find(id);
+            //db.Employees.Remove(employee);
+            db.Entry(employee).State =EntityState.Deleted;
+            db.SaveChanges();
+            //dgv.DataSource = db.Employees.ToList();
+            GridFill();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (isAdd)
+            {
+                var employee = new Employees()
+                {
+                    FirstName = txtName.Text,
+                    LastName =txtSurname.Text,
+                    City = txtCity.Text,
+                    Country = txtCountry.Text
+                };
+                db.Employees.Add(employee);
+                db.Entry(employee).State =EntityState.Added;
+                db.SaveChanges();
+            }
+            else
+            {
+                var employee = db.Employees.Find(id);
+                employee.FirstName = txtName.Text;
+                employee.LastName = txtSurname.Text;
+                employee.City = txtCity.Text;   
+                employee.Country= txtCountry.Text;  
+            }
+            db.SaveChanges();
+            txtName.Text = txtSurname.Text=txtCity.Text=txtCountry.Text ="";
+            groupBox1.Visible=false;
+            //dgv.DataSource = db.Employees.ToList();
+            GridFill();
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            dgv.DataSource = db.Employees.Where(i=>i.FirstName.Contains(txtSearch.Text)|| i.LastName.Contains(txtSearch.Text) || i.City.Contains(txtSearch.Text) || i.Country.Contains(txtSearch.Text)).Select(i=> new
+            {
+                i.FirstName,
+                i.LastName,
+                i.City,
+                i.Country
+            }).ToList();
+        }
+        private void GridFill()
+        {
+          //  var query = from e in db.Employees
+          //              select new
+          //              {
+          //                  e.EmployeeID,
+          //                  e.FirstName,
+          //                  e.LastName,
+          //                  e.City,
+          //                  e.Country
+          //              };
+            //2. yöntem
+            var query2 = db.Employees.Select(e=> new
+            {
+                ID = e.EmployeeID,
+                AD = e.FirstName,
+                SOYAD = e.LastName,
+                ŞEHİR = e.City,
+                ÜLKE = e.Country
+            });
+            dgv.DataSource = query2.ToList();
+        }
+    }
+}
